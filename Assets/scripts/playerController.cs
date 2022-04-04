d0f7d4b27416249dbcab1212ec09f1781da30376
Hector-34 <hector.h.arieta@gmail.com>
2022-04-04 04:39:55 -0500

    second commit

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    bool canJump = true;
+    /*
+    private Transform theTransform;
+    public Vector2 Xrange = Vector2.zero;
+    public Vector2 Yrange = Vector2.zero;
+
+    void LateUpdate(){
+        theTransform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, Yrange.x, Yrange.y),
+            //Mathf.Clamp(transform.position.y, Xrange.x, Xrange.y)
+            //transform.position.z
+        );
+    } 
+    */
+    //---Start is called before the first frame update
+    void Start()
+    {
+        //theTransform = GetComponent<Transform>();
+    }
+
+    //---Update is called once per frame
+    void Update(){
+        
+        if (Input.GetKey("left")){
+            //---------------------- Una unidad (pixel) equivale a un metro
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+        }
+
+         if (Input.GetKey("right")){
+            //---------------------- Una unidad (pixel) equivale a un metro
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+        }
+
+        if (!Input.GetKey("left") && (!Input.GetKey("right"))){
+            gameObject.GetComponent<Animator>().SetBool("moving", false);
+        }
+
+        if (Input.GetKeyDown("up") && canJump){
+            canJump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100f));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.transform.tag == "ground"){
+            canJump = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Contagious19
+## _The Video Game where Covid fights for survive, woe_
+
+
+
+Contagious19 es un videojuego básico para la clase de ✨  Simulación ✨
+
+## Atribuciones de lor recursos
+
+
+https://www.freepik.es/vectores/metro-tren Vector de metro tren creado por macrovector - www.freepik.es
+https://www.freepik.es/vectores/nublado "Vector de nublado creado por macrovector - www.freepik.es
+https://www.flaticon.es/iconos-gratis/vacuna Vacuna iconos creados por Good Ware - Flaticon
+https://www.flaticon.es/iconos-gratis/vacuna Vacuna iconos creados por mynamepong - Flaticon
+https://www.flaticon.es/iconos-gratis/dosis Dosis iconos creados por ultimatearm - Flaticon https://www.flaticon.es/iconos-gratis/alcohol-en-gel Alcohol en gel iconos creados por Freepik - Flaticon
+https://www.flaticon.es/iconos-gratis/alcohol-en-gel Alcohol en gel iconos creados por Kiranshastry - Flaticon
+https://www.flaticon.es/iconos-gratis/jabon Jabón iconos creados por Freepik - Flaticon
+https://www.flaticon.es/iconos-gratis/mascara Máscara iconos creados por Freepik - Flaticon
+https://discoverjamendo.com/es/sync-music/ "Sonidos ambientales obtenidos sin fines de lucro
+
+## Atribución del código
+
+Parcialmente basado en el tutorial de Alva Majo: 
+> Unity para retrasados
+ https://www.youtube.com/c/AlvaMajo/about
